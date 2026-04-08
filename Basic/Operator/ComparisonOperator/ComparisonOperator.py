d94ba1333255fb48b1d51765f21d81a7478eb21e
Kazi-Ashraf-Hossain <ashrafcse020@gmail.com>
2026-04-08 22:45:55 +0600

    Conditional Statement are pending

---
+++ Basic/Operator/ComparisonOperator/ComparisonOperator.py
@@ -0,0 +1,8 @@
+# Equal
+x = 5
+y = 6
+z = 7
+print(x==y)
+
+#Not Equal
+print(y!=z)
---
+++ Basic/Operator/ConditionalStatement/ConditionalStatement.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter a Number: "))
+
+if age >= 18:
+    print(f"You are eligible")
+else:
+    print(f"You aren't eligible")
+
+
